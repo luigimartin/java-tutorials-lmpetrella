3ad78669c43dcd6ae7b2674f924df959b5c4926f
luigimartin.petrella@gmail.com <luigimartin.petrella@gmail.com@006ada52-1292-b0df-6eb9-c8792aa79f33>
2015-01-28 13:50:24 +0000

    New example about how to iterate on HashMap values

---
+++ trunk/EffectiveJava/it/lmpetrella/tutorials/effectivejava/hashmap/HashMapIteratorExample.java
@@ -0,0 +1,32 @@
+//This example is going to show which are the
+//best practice to iterate over HashMap
+
+package it.lmpetrella.tutorials.effectivejava.hashmap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class HashMapIteratorExample {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		
+		for(int i=0; i<10000; i++){
+			//map.put((int) Math.round(Math.random()*10000), UUID.randomUUID().toString().substring(0, 10));
+			//map.put(i, UUID.randomUUID().toString().substring(0, 10));
+			map.put(i, String.valueOf(i));
+			//System.out.println("key: " + i + " value: " + map.get(i));
+		}
+		
+		Iterator<Integer> keySetIterator = map.keySet().iterator();
+		
+		while(keySetIterator.hasNext()){
+		  Integer key = keySetIterator.next();
+		  System.out.println("key: " + key + " value: " + map.get(key));
+		}
+		
+		System.out.println("key: " + 99997 + " value: " + map.get(99997));
+
+	}
+
+}
